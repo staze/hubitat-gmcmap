59cec822f6a88ffe2e508c227dddc12a2d5603d1
Staze <1100483+staze@users.noreply.github.com>
2020-08-17 07:56:38 -0700

    Update hubitat-gmcmap.groovy
    Refactor and updated with code from User12Hubitat here: https://community.hubitat.com/t/help-with-simple-http-query-device-driver/47566/10?u=staze

--- hubitat-gmcmap.groovy
+++ hubitat-gmcmap.groovy
@@ -2,12 +2,14 @@
  * gmcmap.com Geiger Counter Query
  */
 import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
 
 metadata {
     definition(name: "gmcmap.com Geiger Counter", namespace: "staze", author: "Ryan Stasel", importUrl: "https://raw.githubusercontent.com/staze/hubitat-gmcmap/master/hubitat-gmcmap.groovy") {
         capability "Sensor"
-	capability "Refresh"
+	    capability "Polling"
+        attribute "CPM", "NUMBER"
+        attribute "ACPM", "NUMBER"
+        attribute "uSv", "NUMBER"
     }
 }
 
@@ -15,8 +17,8 @@ preferences {
     section("URIs") {
         input "GeigerID", "text", title: "Geiger ID", required: true
         input "Timezone", "text", title: "Timezone", required: false
+	    input name: 'updateMins', type: 'enum', description: "Select the update frequency", title: "Update frequency (minutes)\n0 is disabled", defaultValue: '5', options: ['0', '1', '2', '5','10','15','30'], required: true
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
-	//input name: 'updateMins', type: 'enum', description: "Select the update frequency", title: "${getVersionLabel()}\n\nUpdate frequency (minutes)", displayDuringSetup: true, defaultValue: '5', options: ['1', '2', '3', '5','10','15','30'], required: true
     }
 }
 
@@ -26,30 +28,32 @@ def logsOff() {
 }
 
 def updated() {
+    unschedule()
     log.info "updated..."
     log.warn "debug logging is: ${logEnable == true}"
     if (logEnable) runIn(1800, logsOff)
-    unschedule()
+    if(updateMins != "0") {
+        schedule("0 */${updateMins} * ? * *", poll)
+    }
+    
 }
 
 def parse(String description) {
     if (logEnable) log.debug(description)
 }
 
-def getParams() { "http://www.gmcmap.com/historyData-plain.asp?Param_ID=${GeigerID}&timezone=${Timezone}" }
-
-def refresh() {
-    def responseBody
+def poll() {
+    if (logEnable) log.debug "polling..."
+    def url = "http://www.gmcmap.com/historyData-plain.asp?Param_ID=${GeigerID}&timezone=${Timezone}"
     try {
-	if (logEnable) log.debug "Params:  ${getParams()}"
-	httpGet(getParams()) { resp -> 
-		if (logEnable) log.debug resp.getData()
-		responseBody = resp.getData()
-		state.geiger = jsonSlurper.parseText(resp.data.toString())
+        httpGet(url) { resp -> 
+            if (logEnable) log.debug resp.getData()
+            def respValues = new JsonSlurper().parseText(resp.data.toString().trim())
+            sendEvent(name: "CPM", value: respValues.CPM)
+            sendEvent(name: "ACPM", value: respValues.ACPM)
+            sendEvent(name: "uSv", value: respValues.uSv)
 	}
-		//responseBody = resp.getData()}
     } catch(Exception e) {
-	log.debug "error occured calling httpget ${e}"
+    	log.debug "error occured calling httpget ${e}"
     }
-    if (logEnable) log.info responseBody
 }
